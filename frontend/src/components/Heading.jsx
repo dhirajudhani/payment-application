ff5ccdbeedc7cbb547c64eea3b34c78b3c432604
dhirajudhani <dhirajudhani870@gmail.com>
2024-07-22 11:09:05 +0530

    created components

---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,6 @@
+
+export const Heading = ({label}) => {
+    return <div className="text-4xl font-bold pt-6">
+        {label}
+    </div>
+}
---
+++ frontend/src/components/InputBox.jsx
@@ -0,0 +1,6 @@
+export const InputBox = ({placeholder, label}) => {
+    return <div>
+       <div className="text-sm font-medium text-left py-2">{label}</div>
+       <input type="text" placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200"/>
+    </div>
+}
